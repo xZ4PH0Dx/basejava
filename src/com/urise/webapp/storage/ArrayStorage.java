0e93018dd96f30538991583a6420d1879d9314d0
Ruslan <dreamfall3r@gmail.com>
2020-07-31 01:11:09 +0300

    изменил в соответствии с коммитами

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -10,8 +10,9 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private static final int MAX_STORAGE_SIZE = 10000;
+    private final Resume[] storage = new Resume[MAX_STORAGE_SIZE];
     private int lastItem = 0;
-    private Resume[] storage = new Resume[MAX_STORAGE_SIZE];
+
 
     /**
      * Clear storage
@@ -25,15 +26,9 @@ public class ArrayStorage {
      * Saves resume to storage
      */
     public void save(Resume r) {
-        boolean isExist = false;
         if (lastItem < MAX_STORAGE_SIZE) {
-            for (int i = 0; i < lastItem; i++) {
-                if (r.getUuid().equals(storage[i].getUuid())) {
-                    isExist = true;
-                    break;
-                }
-            }
-            if (!isExist) {
+            int index = getResumeIndex(r.getUuid());
+            if (index == -1) {
                 storage[lastItem] = r;
                 lastItem++;
             } else {
@@ -48,41 +43,41 @@ public class ArrayStorage {
      * @return com.urise.webapp.model.Resume by uuid. Null if not exists such uuid
      */
     public Resume get(String uuid) {
-        for (int i = 0; i < lastItem; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        int index = getResumeIndex(uuid);
+        if (index != -1) {
+            return storage[index];
+        } else {
+            printError("get");
+            return null;
         }
-        printError("get");
-        return null;
     }
 
     /**
      * Delete uuid from storage
      */
     public void delete(String uuid) {
-        for (int i = 0; i < lastItem; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[lastItem - 1];
-                storage[size() - 1] = null;
-                lastItem--;
-                return;
-            }
+        int index = getResumeIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[lastItem - 1];
+            storage[size() - 1] = null;
+            lastItem--;
+            return;
+        } else {
+            printError("delete");
         }
-        printError("delete");
     }
 
     /**
      * Update Resume
      */
     public void update(Resume resume) {
-        for (int i = 0; i < lastItem; i++) {
-            if (storage[i].getUuid().equals(resume.getUuid())) {
-                storage[i] = resume;
-                return;
-            }
+        int index = getResumeIndex(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+            return;
+        } else {
+            printError("update");
         }
-        printError("update");
     }
 
     /**
@@ -101,5 +96,14 @@ public class ArrayStorage {
 
     public void printError(String string) {
         System.out.println("Couldn't " + string + " Resume. No such Resume");
+    }
+
+    private int getResumeIndex(String uuid) {
+        for (int i = 0; i < lastItem; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
